8129ca8cce08f142e98e5bbaeab8d936f11489f1
avtishin <picmymxybrim5xybsI>
2023-05-16 00:20:42 +0400

    add small whois

--- wachter/model.py
+++ wachter/model.py
@@ -15,16 +15,10 @@ class Chat(Base):
 
     id = Column(BigInteger, primary_key=True)
 
-    on_new_chat_member_message = Column(Text, nullable=False, default='Привет %USER_MENTION%, Это Мишпуха 2.0 без ненависти. ' + \
-                                                                      'Напиши пару слов (30 символов) с тегом #whois, кто ты и чем занимаешься.' + \
-                                                                      ' Мы хотим создавать сообщество для студентов, выпускников,' + \
-                                                                      ' сотрудников и друзей РЭШ, поэтому, ' + \
-                                                                      'если ты не представишься в течение 30 минут, ' + \
-                                                                      'мы будем вынуждены тебя удалить из чата.')
+    on_new_chat_member_message = Column(Text, nullable=False, default='Привет %USER_MENTION%, Это Мишпуха 2.0 без ненависти. Напиши пару слов (30 символов) с тегом #whois, кто ты и чем занимаешься. Мы хотим создавать сообщество для студентов, выпускников, сотрудников и друзей РЭШ, поэтому, если ты не представишься в течение 30 минут, мы будем вынуждены тебя удалить из чата.')
     on_known_new_chat_member_message = Column(Text, nullable=False, default='Добро пожаловать. Снова')
     on_introduce_message = Column(Text, nullable=False, default='Добро пожаловать.')
-    on_introduce_message_small_whois = Column(Text, nullable=False, default='%USER\_MENTION%, твое сообщение слишком короткое, '
-                                                                            'напиши о себе подробнее.')
+    on_introduce_message_small_whois = Column(Text, nullable=False, default='%USER\_MENTION%, твое сообщение слишком короткое, напиши о себе подробнее.')
     on_kick_message = Column(Text, nullable=False, default='%USER\_MENTION% молчит и покидает чат')
     notify_message = Column(Text, nullable=False, default='%USER\_MENTION%, пожалуйста, представьтесь и поздоровайтесь с сообществом.')
     regex_filter = Column(Text, nullable=True)
