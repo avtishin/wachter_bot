e0c34aca7d58a0ba9c89c62bdc6a3ecfec32744b
avtishin <picmymxybrim5xybsI>
2023-05-15 22:50:32 +0400

    add time out

--- wachter/constants.py
+++ wachter/constants.py
@@ -36,9 +36,9 @@ Regex фильтр: ```{regex_filter}```
 Кикать по regex только новых: {filter_only_new_users}
 """
 
-default_kick_timeout = 0
+default_kick_timeout = 30
 notify_delta = 10
-min_whois_length = 60
+min_whois_length = 20
 
 
 # ACTIONS
