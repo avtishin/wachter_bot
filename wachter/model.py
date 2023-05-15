6253c12b0349a7fce960773ac35d4830eea05e17
avtishin <picmymxybrim5xybsI>
2023-05-16 00:24:04 +0400

    add small whois

--- wachter/model.py
+++ wachter/model.py
@@ -19,7 +19,7 @@ class Chat(Base):
     on_known_new_chat_member_message = Column(Text, nullable=False, default='Добро пожаловать. Снова')
     on_introduce_message = Column(Text, nullable=False, default='Добро пожаловать.')
     on_introduce_message_small_whois = Column(Text, nullable=False, default='%USER\_MENTION%, твое сообщение слишком короткое, напиши о себе подробнее.')
-on_kick_message = Column(Text, nullable=False, default='%USER\_MENTION% молчит и покидает чат')
+    on_kick_message = Column(Text, nullable=False, default='%USER\_MENTION% молчит и покидает чат')
     notify_message = Column(Text, nullable=False, default='%USER\_MENTION%, пожалуйста, представьтесь и поздоровайтесь с сообществом.')
     regex_filter = Column(Text, nullable=True)
     filter_only_new_users = Column(Boolean, nullable=False, default=False)
